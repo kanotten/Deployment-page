526a68fefcd80646c3ada42331630396ffff787c
Sheikh <kenneth.sheikh@hotmail.com>
2024-10-04 11:06:09 +0200

    added javascript and button with event click

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+// JS/app.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("button");
+
+  button.addEventListener("click", () => {
+    alert("Good job!");
+  });
+});
