c2be871b1cd67e6a27cc601938209f3634f296df
Sheikh <kenneth.sheikh@hotmail.com>
2024-10-05 17:25:01 +0200

    fixed html files in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import path from "path";
 
@@ -8,12 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, "index.html"),
-        login: path.resolve(__dirname, "auth/login/index.html"),
-        register: path.resolve(__dirname, "auth/register/index.html"),
-        profile: path.resolve(__dirname, "profile/index.html"),
-        post: path.resolve(__dirname, "post/index.html"),
-        postCreate: path.resolve(__dirname, "post/create/index.html"),
-        postEdit: path.resolve(__dirname, "post/edit/index.html"),
+        dynamic: path.resolve(__dirname, "dynamic.html"),
       },
     },
   },
